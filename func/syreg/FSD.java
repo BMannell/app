662e8a1fdd6a6ddcba68137ecce95f63a75a82c8
BMannell <benjamin.mannell@gmail.com>
2015-04-15 22:44:58 -0400

    Symbolic Regression
    -added symbolic regression problem and functions
    -added boolean to project data
    -if, great, and less now use boolean
    -made script to combine stat files

---
+++ func/syreg/FSD.java
@@ -0,0 +1,24 @@
+package ec.app.func.syreg;
+import ec.app.SyRegProblem;
+import ec.app.ProjectData;
+import ec.*;
+import ec.gp.*;
+import ec.util.*;
+
+public class FSD extends GPNode
+{
+    public String toString() { return "FreeSulfurDioxide"; }
+
+    public int expectedChildren() { return 0; }
+
+    public void eval(final EvolutionState state,
+       final int thread,
+       final GPData input,
+       final ADFStack stack,
+       final GPIndividual individual,
+       final Problem problem)
+    {
+        ProjectData rd = ((ProjectData)(input));
+        rd.x = ((SyRegProblem)problem).currentFSD;
+    }
+}
